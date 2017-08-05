d160732537e0fbbd34970b817af606a7f1667e34
GitTrolls <theaaronbyte@gmail.com>
2017-08-05 11:55:41 +0100

    Avoid early return

--- src/options/dir_action.rs
+++ src/options/dir_action.rs
@@ -1,40 +1,23 @@
-use getopts;
+use options::parser::Matches;
+use options::{flags, Misfire};
 
-use options::misfire::Misfire;
+use fs::dir_action::{DirAction, RecurseOptions};
 
-
-/// What to do when encountering a directory?
-#[derive(PartialEq, Debug, Copy, Clone)]
-pub enum DirAction {
-
-    /// This directory should be listed along with the regular files, instead
-    /// of having its contents queried.
-    AsFile,
-
-    /// This directory should not be listed, and should instead be opened and
-    /// *its* files listed separately. This is the default behaviour.
-    List,
-
-    /// This directory should be listed along with the regular files, and then
-    /// its contents should be listed afterward. The recursive contents of
-    /// *those* contents are dictated by the options argument.
-    Recurse(RecurseOptions),
-}
 
 impl DirAction {
 
     /// Determine which action to perform when trying to list a directory.
-    pub fn deduce(matches: &getopts::Matches) -> Result<DirAction, Misfire> {
-        let recurse = matches.opt_present("recurse");
-        let list    = matches.opt_present("list-dirs");
-        let tree    = matches.opt_present("tree");
+    pub fn deduce(matches: &Matches) -> Result<DirAction, Misfire> {
+        let recurse = matches.has(&flags::RECURSE);
+        let list    = matches.has(&flags::LIST_DIRS);
+        let tree    = matches.has(&flags::TREE);
 
         match (recurse, list, tree) {
 
             // You can't --list-dirs along with --recurse or --tree because
             // they already automatically list directories.
-            (true,  true,  _    )  => Err(Misfire::Conflict("recurse", "list-dirs")),
-            (_,     true,  true )  => Err(Misfire::Conflict("tree", "list-dirs")),
+            (true,  true,  _    )  => Err(Misfire::Conflict(&flags::RECURSE, &flags::LIST_DIRS)),
+            (_,     true,  true )  => Err(Misfire::Conflict(&flags::TREE,    &flags::LIST_DIRS)),
 
             (_   ,  _,     true )  => Ok(DirAction::Recurse(RecurseOptions::deduce(matches, true)?)),
             (true,  false, false)  => Ok(DirAction::Recurse(RecurseOptions::deduce(matches, false)?)),
@@ -42,45 +25,15 @@ impl DirAction {
             (false, false, _    )  => Ok(DirAction::List),
         }
     }
-
-    /// Gets the recurse options, if this dir action has any.
-    pub fn recurse_options(&self) -> Option<RecurseOptions> {
-        match *self {
-            DirAction::Recurse(opts) => Some(opts),
-            _ => None,
-        }
-    }
-
-    /// Whether to treat directories as regular files or not.
-    pub fn treat_dirs_as_files(&self) -> bool {
-        match *self {
-            DirAction::AsFile => true,
-            DirAction::Recurse(RecurseOptions { tree, .. }) => tree,
-            _ => false,
-        }
-    }
 }
 
-
-/// The options that determine how to recurse into a directory.
-#[derive(PartialEq, Debug, Copy, Clone)]
-pub struct RecurseOptions {
-
-    /// Whether recursion should be done as a tree or as multiple individual
-    /// views of files.
-    pub tree: bool,
-
-    /// The maximum number of times that recursion should descend to, if one
-    /// is specified.
-    pub max_depth: Option<usize>,
-}
 
 impl RecurseOptions {
 
     /// Determine which files should be recursed into.
-    pub fn deduce(matches: &getopts::Matches, tree: bool) -> Result<RecurseOptions, Misfire> {
-        let max_depth = if let Some(level) = matches.opt_str("level") {
-            match level.parse() {
+    pub fn deduce(matches: &Matches, tree: bool) -> Result<RecurseOptions, Misfire> {
+        let max_depth = if let Some(level) = matches.get(&flags::LEVEL) {
+            match level.to_string_lossy().parse() {
                 Ok(l)   => Some(l),
                 Err(e)  => return Err(Misfire::FailedParse(e)),
             }
@@ -90,15 +43,5 @@ impl RecurseOptions {
         };
 
         Ok(RecurseOptions { tree, max_depth })
-    }
-
-    /// Returns whether a directory of the given depth would be too deep.
-    pub fn is_too_deep(&self, depth: usize) -> bool {
-        match self.max_depth {
-            None    => false,
-            Some(d) => {
-                d <= depth
-            }
-        }
     }
 }
