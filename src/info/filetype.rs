161044a526e7d5018420826fe45f76a64c30decb
GitTrolls <theaaronbyte@gmail.com>
2021-01-03 22:44:16 +0100

    Use same styling for spaces between icon and file name

--- src/info/filetype.rs
+++ src/info/filetype.rs
@@ -28,7 +28,7 @@ impl FileExtensions {
             "Gruntfile.coffee", "BUILD", "BUILD.bazel", "WORKSPACE", "build.xml",
             "webpack.config.js", "meson.build", "composer.json", "RoboFile.php", "PKGBUILD",
             "Justfile", "Procfile", "Dockerfile", "Containerfile", "Vagrantfile", "Brewfile",
-            "Gemfile", "Pipfile", "build.sbt", "mix.exs", "bsconfig.json", "tsconfig.json",
+            "Gemfile", "Pipfile", "build.sbt", "mix.exs",
         ])
     }
 
